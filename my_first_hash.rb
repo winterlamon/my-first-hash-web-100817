94f2af385219094d90d2af7f4336112380fd95e3
Winter LaMon <winter.lamon@gmail.com>
2017-10-01 03:30:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,14 +9,11 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here
-  #remember to return the shipping_manifest hash
-
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 end
